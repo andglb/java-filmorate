48abef4b64cc3d5159fbc1cb43e3df21e29bbf9a
Андрей <andrew-g0lub@yandex.ru>
2023-05-15 20:58:22 +0300

    Спринт №10.3

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface UserStorage {
     List<User> getUsers();
+
     User create(User user);
+
     User update(User user);
+
     User getUserById(Long userId);
 }
