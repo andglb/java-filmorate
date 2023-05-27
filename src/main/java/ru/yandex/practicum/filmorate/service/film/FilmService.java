95f073498e133f58d56368520e03a44d2d82024e
Андрей <andrew-g0lub@yandex.ru>
2023-05-27 15:19:56 +0300

    Промежуточное задание

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
